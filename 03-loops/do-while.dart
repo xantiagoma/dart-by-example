cf9296694bbc899966640b351dfdc4660aea6f35
Santiago Montoya <smontoya@hugeinc.com>
2019-08-12 12:07:15 -0500

    loops

---
+++ 03-loops/do-while.dart
@@ -0,0 +1,8 @@
+main() {
+  var j = 0;
+
+  // do-while conditions are evaluated after the loop
+  do {
+    print("dowhile $j");
+  } while (j++ < 2);
+}
---
+++ 03-loops/while.dart
@@ -0,0 +1,10 @@
+main() {
+  var i = 0;
+
+  // while conditions are evaluated before the loop
+  while(i++ < 2) {
+    print("while $i");
+  }
+
+  var j = 0;
+}
